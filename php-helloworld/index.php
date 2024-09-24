fda216404ca8869deb29525c7e48476be4cbbf4e
Student User <student@workstation.lab.example.com>
2024-09-24 19:25:33 -0400

    update

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, to my friends around the world! I am a PHP application using PHP version " . PHP_VERSION . "\n";
+print " second Hello, to my friends around the world! I am a PHP application using PHP version " . PHP_VERSION . "\n";
 ?>
